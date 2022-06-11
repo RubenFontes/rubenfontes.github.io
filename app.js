b539b3ac2af483efdecc231762813f5e37672a31
RubenFontes <rubencfontes17@gmail.com>
2022-06-11 16:15:05 -0300

    add event click

---
+++ app.js
@@ -0,0 +1,7 @@
+var div = document.getElementById("container");
+var button = document.getElementById("btn");
+
+button.onclick = ()=>{
+    button.style.display = "none";
+    div.style.display = "block";
+}
