f9c8ba0ec103675d5b250aff06b87b2d46a45025
RubenFontes <rubencfontes17@gmail.com>
2022-06-12 19:56:56 -0300

    deploy

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var div = document.getElementById("container");
 var button = document.getElementById("btn");
 var img = document.getElementById("img");
 
-button.onclick = ()=>{
-    
-    img.setAttribute("src", "assets/img/down.png")
 
-    if (div.style.display === 'none') {
-        div.style.display = 'block';
-      } else {
-        div.style.display = 'none';
-        img.setAttribute("src", "assets/img/up.png")
-      }
+button.onmouseover = () => button.style.textDecoration = "underline";
+button.onmouseout = () => button.style.textDecoration = "none";
+
+button.onclick = () => {
+    
+img.setAttribute("src", "assets/img/minus.png");
+
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+    img.setAttribute("src", "assets/img/plus.png");
+  }
 }
+
