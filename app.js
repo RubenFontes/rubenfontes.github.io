5f54f081f3d7bc443dce3cce26ed271ac8833507
RubenFontes <rubencfontes17@gmail.com>
2022-06-11 21:49:39 -0300

    accordion

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var div = document.getElementById("container");
 var button = document.getElementById("btn");
+var img = document.getElementById("img");
 
 button.onclick = ()=>{
-    button.style.display = "none";
-    div.style.display = "block";
+    
+    img.setAttribute("src", "assets/img/down.png")
+
+    if (div.style.display === 'none') {
+        div.style.display = 'block';
+      } else {
+        div.style.display = 'none';
+        img.setAttribute("src", "assets/img/up.png")
+      }
 }
