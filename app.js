91bbf99acc34e109d5cf4d71e999520956ea9af5
RubenFontes <rubencfontes17@gmail.com>
2022-12-27 22:40:06 -0300

    add link & fix color

---
+++ app.js
@@ -0,0 +1,20 @@
+var div = document.getElementById("container");
+var button = document.getElementById("btn");
+var img = document.getElementById("img");
+
+
+button.onmouseover = () => button.style.textDecoration = "underline";
+button.onmouseout = () => button.style.textDecoration = "none";
+
+button.onclick = () => {
+    
+img.setAttribute("src", "assets/img/minus.png");
+
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+    img.setAttribute("src", "assets/img/plus.png");
+  }
+}
+
