1054a18f1bc4ee311c8243712837652edbb4390f
Ruben Fontes <55508678+RubenFontes@users.noreply.github.com>
2023-04-21 02:54:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ var button = document.getElementById("btn");
 var img = document.getElementById("img");
 
 
-button.onmouseover = () => button.style.textDecoration = "underline";
-button.onmouseout = () => button.style.textDecoration = "none";
+button.onmouseover = () => button.style.textDecoration = "none";
+button.onmouseout = () => button.style.textDecoration = "underline";
 
 button.onclick = () => {
     
-img.setAttribute("src", "assets/img/minus-dark.png");
+img.setAttribute("src", "assets/img/minus2.png");
 
   if (div.style.display === "none") {
     div.style.display = "block";
   } else {
     div.style.display = "none";
-    img.setAttribute("src", "assets/img/plus-dark.png");
+    img.setAttribute("src", "assets/img/plus2.png");
   }
 }
 
